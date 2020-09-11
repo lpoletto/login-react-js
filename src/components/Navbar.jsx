846b82f641b3b889e2d5d6ea7d836b628432c599
lpoletto <48451077+lpoletto@users.noreply.github.com>
2020-09-10 22:23:18 -0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,13 +17,13 @@ const Navbar = (props) => {
     return (
         <div className="navbar navbar-dark bg-dark">
             
-            <Link to="/login" className="navbar-brand">AUTH</Link>
+            <Link to="/" className="navbar-brand">AUTH</Link>
 
             <div>
                 <div className="d-flex">
-                    {/* <NavLink to="/" exact className="btn btn-dark mr-2">
+                    <NavLink to="/" exact className="btn btn-dark mr-2">
                         Inicio
-                    </NavLink> */}
+                    </NavLink>
                     {
                         props.firebaseUser !== null ? (
                             <NavLink to="/admin" className="btn btn-dark mr-2">
